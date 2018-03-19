a1287a70748fac05d759cd390a2e8f38414b61a1
MartinHinz <martin.hinz@ufg.uni-kiel.de>
2018-03-19 11:17:59 +0100

    added second part to handout

--- handout.R
+++ handout.R
@@ -112,3 +112,133 @@ my_result_data
 # 2. c14bazAAR #
 ################
 
+# ---------------- your code ----------------
+
+# fill in your code here
+
+# ---------------- your code ----------------
+
+##########################################
+#
+# The following snippet filters a c14datelist for dates within a range of (4000 - 4500 calBP).
+#
+##########################################
+
+# ---------------- snip ----------------
+
+dates_4000_4500 <- calibrated %>%
+  dplyr::select(labnr, calrange) %>%
+  tidyr::unnest() %>%
+  dplyr::filter(
+    from >= 4000 & from <= 4500 |
+      to >= 4000 & to <= 4500
+  ) %$%
+  labnr %>%
+  unique
+
+calibrated[calibrated$labnr %in% dates_4000_4500, ]
+
+# ---------------- snip ----------------
+
+# ---------------- your code ----------------
+
+# fill in your code here
+
+# ---------------- your code ----------------
+
+##########################################
+#
+# The following snippet get all the dates of Baden-Würtemberg within the range of 7500 and 2700 cal BP (!). 
+#
+##########################################
+
+# ---------------- snip ----------------
+
+#### get all dates (again)
+
+all_dates <- c14bazAAR::get_all_dates()
+
+#### download shapefile from Baden-Württemberg ####
+
+# create a tempfile
+temp <- tempfile()
+
+# download archive with shapefile
+utils::download.file(
+  "https://www.lgl-bw.de/lgl-internet/web/sites/default/de/07_Produkte_und_Dienstleistungen/Open_Data_Initiative/Galerien/Dokumente/AX_Gebiet_Bundesland.zip", 
+  temp, 
+  quiet = TRUE
+)
+
+# unzip archive in tempdir
+utils::unzip(
+  temp,
+  exdir = tempdir()
+)
+
+# read shapefile into R
+bawue_sf <- st_read(file.path(tempdir(), "AX_Gebiet_Bundesland.shp"))
+
+plot(bawue_sf$geometry)
+
+#### prepare date dataset ####
+
+# reduce date selection (to reduce calculation time)
+sw_germany_dates_1 <- all_dates %>%
+  dplyr::filter(lat < 50, lat > 47, lon < 11, lon > 7.5)
+
+sw_germany_dates_2 <- sw_germany_dates_1 %>%
+  as.c14_date_list() %>%
+  classify_material() %>%
+  dplyr::filter(material_thes != "charcoal", material_thes != "wood")
+
+sw_germany_dates_3 <- sw_germany_dates_2 %>%
+  as.c14_date_list() %>%
+  determine_country_by_coordinate() %>%
+  dplyr::filter(country_coord == "Germany")
+
+sw_germany_dates_4 <- sw_germany_dates_3 %>%
+  as.c14_date_list() %>%
+  remove_duplicates()
+
+# make c14_date_list a sf object and transform coordinates to ETRS89 / UTM zone 32N reference system
+sw_germany_dates_sf <- sw_germany_dates_4 %>%
+  as.c14_date_list()%>%
+  as.sf %>%
+  st_transform(crs = 25832)
+
+plot(sw_germany_dates_sf$geom, add = TRUE, col = "black")
+
+#### intersect shape with dates ####
+
+# spatial intersection to find dates within Baden-Württemberg
+bawue_dates_sf <- sw_germany_dates_sf %>%
+  st_intersection(y = bawue_sf)
+
+plot(bawue_dates_sf$geom, add = TRUE, col = "red")
+
+# transform this sf file to a data.frame
+bawue_dates <- bawue_dates_sf
+st_geometry(bawue_dates) <- NULL
+colnames(bawue_dates) <- gsub("data.", "", colnames(bawue_dates))
+
+#### final selection by timeframe
+
+# calibrate the dates in this selection
+bawue_calibrated <- bawue_dates %>%
+  as.c14_date_list() %>%
+  calibrate(choices = "calprobdistr")
+
+# select dates by timeframe
+bawue_final <- bawue_calibrated %>%
+  unnest() %>%
+  dplyr::filter(
+    calage > 2700, calage < 7500
+  ) %>%
+  dplyr::group_by(labnr) %>%
+  dplyr::do(head(., n = 1)) %>% 
+  dplyr::ungroup()
+
+#saveRDS(bawue_final, file = "bawue_final.RDS")
+
+# ---------------- snip ----------------
